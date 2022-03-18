78f882eb72360a24eb88afe95ac86a57930d377e
Ammar Alhasan <87602055+ammarCashewP@users.noreply.github.com>
2022-03-18 15:55:28 +0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -7,5 +7,8 @@ module.exports = ({ env }) => ({
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
+    options: {
+      ssl: env.bool('DATABASE_SSL', false),
+    },
   },
 });
